081be4d67fd2bbd9040eee7dac3aa7f05fc39627
Andris Pavēnis <andris.pavenis@fmi.fi>
2025-07-01 11:59:28 +0300

    Make actual engine implementation derived class

--- authentication/Engine.h
+++ authentication/Engine.h
@@ -2,6 +2,7 @@
 
 #include "Config.h"
 #include <spine/SmartMetEngine.h>
+#include <macgyver/Exception.h>
 #include <memory>
 #include <string>
 #include <vector>
@@ -12,10 +13,10 @@ namespace Engine
 {
 namespace Authentication
 {
-class Engine final : public SmartMet::Spine::SmartMetEngine
+class Engine : public SmartMet::Spine::SmartMetEngine
 {
  public:
-  explicit Engine(const char* theConfigFile);
+  Engine() {};
   ~Engine() override = default;
 
   Engine(const Engine& other) = delete;
@@ -23,25 +24,37 @@ class Engine final : public SmartMet::Spine::SmartMetEngine
   Engine(Engine&& other) = delete;
   Engine& operator=(Engine&& other) = delete;
 
+  virtual bool isEnabled() const
+  {
+    return false;  // Default dummy implementation for disabled engine
+  }
+
   // Query if given apikey has access to a number of token values for a given service
-  bool authorize(const std::string& apikey,
+  virtual bool authorize(const std::string& apikey,
                  const std::vector<std::string>& tokenvalues,
-                 const std::string& service) const;
+                 const std::string& service) const
+  {
+    (void)apikey;
+    (void)tokenvalues;
+    (void)service;
+    throw Fmi::Exception(BCP, "Not implemented");
+  }
 
   // Authorize a single value for given service
-  bool authorize(const std::string& apikey,
+  virtual bool authorize(const std::string& apikey,
                  const std::string& tokenvalue,
                  const std::string& service,
-                 bool explicitGrantOnly = false) const;
-
- protected:
-  void init() override;
-  void shutdown() override;
-
- private:
-  class Impl;
-  std::unique_ptr<Impl> impl;
-  Impl& get_impl() const;
+                 bool explicitGrantOnly = false) const
+  {
+    (void)apikey;
+    (void)tokenvalue;
+    (void)service;
+    (void)explicitGrantOnly;
+    throw Fmi::Exception(BCP, "Not implemented");
+  }
+protected:
+  void init() override {}
+  void shutdown() override {}
 };
 
 }  // namespace Authentication
